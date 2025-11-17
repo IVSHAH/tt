433043ba8dad98e085499586ad6ff427604166b9
IVSHAH <shashovivan2@mail.ru>
2025-11-17 12:44:33 +0300

    feat: add user entity

---
+++ src/features/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('numeric', { precision: 12, scale: 2, default: 0 })
+  balance: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
