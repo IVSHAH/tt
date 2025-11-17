dd2b9f4cafdba4d3e163c12cea09a6c6d404aad5
IVSHAH <shashovivan2@mail.ru>
2025-11-17 13:19:40 +0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostgresModule } from './providers/databases/postgres.module';
+import { UsersModule } from './features/users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PostgresModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PostgresModule, UsersModule],
 })
 export class AppModule {}
